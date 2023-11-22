c8425e4b50807319627d930451e2176e74906bde
Soaen <sylvain.vansteenkiste59@gmail.com>
2023-11-22 13:55:50 +0100

    🐸 Add routes for noises by categories

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    
     {
       path: '/',
       name: 'home',
@@ -58,7 +59,7 @@ const router = createRouter({
       name: 'detailnoises',
       component: () => import('../views/DetailNoisesView.vue')
     }
-    
+
   ]
 })
 
